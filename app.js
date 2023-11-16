fcf0bc795e9d326527730ef5676a914d675ff6cc
Ellie <eagyeman1234@gmail.com>
2023-11-15 23:25:54 -0500

    style clock

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 // console.log("hello") constants are variables that you define at the top of your program that do NOT change!!
 
 
-
 /*---------------------------- Variables (state) ----------------------------*/
 
 
@@ -14,8 +13,8 @@
 // A *cached element reference* is a technique in which a DOM element is stored in a variable for future use, minimizing the need to repeatedly query the DOM to access the same element, thus improving performance in web apps.
 
 const secondHand = document.querySelector('.second-hand')
-const minsHand = document.querySelector('minute-hand')
-const hourHand = document.querySelector('hour-hand')
+const minsHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
 
 
 
@@ -35,7 +34,7 @@ function setDate() {
   const mins = now.getMinutes()
   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`
-
+  
   const hour = now.getHours()
   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90
   hourHand.style.transform = `rotate(${hourDegrees}deg)`
