cb0fb4e813a21e15a076eba67daa6949a798a6e2
Ellie <eagyeman1234@gmail.com>
2023-11-15 22:43:46 -0500

    test app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+
+/*-------------------------------- Constants --------------------------------*/
+// console.log("hello")
+
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+// A *cached element reference* is a technique in which a DOM element is stored in a variable for future use, minimizing the need to repeatedly query the DOM to access the same element, thus improving performance in web apps.
+
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
