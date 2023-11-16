dec70b2d230293ad36fa7c16d60dfb640ae5f64c
Ellie <eagyeman1234@gmail.com>
2023-11-15 23:47:30 -0500

    fix error

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const hourHand = document.querySelector('.hour-hand')
     const now = new Date()
     
     const seconds = now.getSeconds()
-    const secondsDegrees = ((seconds / 60) * 360) + ((seconds/60)*6) + 90
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
     
     const mins = now.getMinutes()
